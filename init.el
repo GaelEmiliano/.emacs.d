da177a423515dee903ed3e5e6d45d453627b89b5
GaelEmiliano <EmilianoArreguin077@ciencias.unam.mx>
2024-11-09 15:11:46 -0600

    Added lisp directory

--- init.el
+++ init.el
@@ -1,113 +1,136 @@
-;; Desactiva la barra de herramientas
+;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
+;;; Commentary:
+;; This is the main configuration file for Emacs (init.el) where
+;; packages are loaded and personal preferences are defined.
+
+;;; Code:
+
+;; ---------------------------------
+;; BASIC INTERFACE SETTINGS
+;; ---------------------------------
+
+;; Disable the startup screen
+(setq inhibit-startup-screen t)
+
+;; Disable the menu bar
 (menu-bar-mode -1)
 
-;; Desactiva la barra de menú
+;; Disable the tool bar
 (tool-bar-mode -1)
 
-;; Muestra la barra de desplazamiento
-(scroll-bar-mode t)
+;; Disable the scroll bar
+(scroll-bar-mode -1)
 
-;; Añadimos las líneas a la izquierda
+;; Show line numbers on the left side
 (global-display-line-numbers-mode 1)
-;; Usa números de línea relativos
+
+;; Use relative line numbers for better navigation
 (setq display-line-numbers-type 'relative)
 
-;; Cambia el tipo de letra a "Dejavu Sans Mono 11"
+;; Set font to "DejaVu Sans Mono, size 11"
 (set-face-attribute 'default nil :font "DejaVu Sans Mono-11")
 
-;; Configuración de repositorios de paquetes
-;; Package management
+;; ---------------------------------
+;; PACKAGE MANAGEMENT
+;; ---------------------------------
+
+;; Set up package repositories (MELPA and ELPA)
 (require 'package)
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-	("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+	                 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-(package-refresh-contents)
-;; Actualiza la lista de paquetes si no está ya disponible
+
+;; Refresh package list if necessary
 (unless package-archive-contents
   (package-refresh-contents))
-;; Instala si no está ya instalado
+
+;; Install use-package if it's not installed
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
+;; Always ensure use-package installs missings packages
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Indentacion de C y C++
-(defun my-c-mode-hook()
-  (setq c-basic-offset 4)
-  (setq tab-width 4)
-  (setq indent-tabs-mode nil))
-;; Establece la nueva indentacion a 4 espacios
-(add-hook 'c-mode-common-hook 'my-c-mode-hook)
+;; ---------------------------------
+;; KEYBINDINGS AND FUNCTIONS
+;; ---------------------------------
 
-;; Indentacion de Python
-(defun my-python-mode-hook()
-  (setq python-indent-offset 4)
-  (setq tab-width 4)
-  (setq indent-tabs-mode nil))
-;; Establece la nueva indentacion a 4 espacios
-(add-hook 'python-mode-hook 'my-python-mode-hook)
+;; Move to the next buffer
+(global-set-key (kbd "C-x C-?") 'next-buffer)
 
-;; Configuración de Golang
-(use-package go-mode
-  :ensure t
-  :mode "\\.go\\'"
-  :config
-  (setq tab-width 4) ;; Ancho de tabulación a 4 espacios
-  (setq gofmt-command "gofmt") ;; Usar gofmt para formatear el código
-  (add-hook 'before-save-hook 'gofmt-before-save) ;; Formatear
-  (setq indent-tabs-mode nil) ;, Usar espacios en lugar de tabs
-  (setq go-tab-width 4)) ;; Especificamente para Go
+;; Move to the previous buffer
+(global-set-key (kbd "C-x C-p") 'previous-buffer)
 
-;; Configuración de Rust
-(use-package rust-mode
-  :ensure t
-  :mode "\\.rs\\'"
-  :config
-  (setq rust-format-on-save t))
+;; Switch between windows in reverse
+(global-set-key (kbd "C-x C-o") 'other-window-backward)
 
-;; Configuración de Ruby
-(use-package ruby-mode
-  :ensure t
-  :mode "\\.rb\\'"
-  :interpreter "ruby")
+;; Comment, uncomment, or add a comment at the end of the line
+(global-set-key (kbd "C-x ,") 'comment-dwim)
 
-;; Configuración de Kotlin
-(use-package kotlin-mode
-  :ensure t
-  :mode "\\.kt\\'")
+;; Scroll N lines behind
+(global-set-key (kbd "C-q") 'scroll-n-lines-behind)
 
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook ((go-mode . lsp-deferred)
-         (rust-mode . lsp-deferred)))
+;; Scroll N lines ahead
+(global-set-key (kbd "C-z") 'scroll-n-lines-ahead)
 
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode)
+;; Switch buffers using Ivy or Counsel
+(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
 
-;; Autocompletado con company
+;; Magit status
+;;(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Comment or uncomment a single line
+(global-set-key (kbd "C-c C-f") 'comment-line)
+
+;; Comment or uncomment a region
+(global-set-key (kbd "C-c C-r") 'comment-or-uncomment-region)
+
+;; Duplicate the current line
+(global-set-key (kbd "C-c d") 'duplicate-line)
+
+;; Switch between windows in reverse function definition
+(defun other-window-backward(&optional n)
+  "Select Nth previous window"
+  (interactive)
+  (other-window (- (prefix-numeric-value n))))
+
+;; Scroll N lines behind function definition
+(defun scroll-n-lines-behind(&optional n)
+  "Scroll behind N lines"
+  (interactive)
+  (scroll-behind (prefix-numeric-value n)))
+
+;; Scroll N lines ahead function definition
+(defun scroll-n-lines-ahead(&optional n)
+  "Scroll ahead N lines"
+  (interactive)
+  (scroll-ahead (prefix-numeric-value n)))
+
+;; Duplicate line function definition
+(defun duplicate-line()
+  "Duplicate current line"
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
+
+;; ---------------------------------
+;;  SOME PACKAGES
+;; ---------------------------------
+
+;; Magit
+;;(use-package magit)
+
+;; Autocompletado con company 
 (use-package company
   :ensure t
-  :init (global-company-mode 1))
-
-;;(use-package flycheck
-;;  :ensure t
-;;  :init (global-flycheck-mode 1))
-
-;;(use-package yasnippet
-;;  :ensure t
-;;  :init (yas-global-mode 1))
-
-;; Haskell-mode
-(use-package haskell-mode)
-
-;; PHP-mode
-(use-package php-mode)
-
-;; Racket-mode
-(use-package racket-mode)
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  (add-to-list 'company-backends 'company-clang))
 
 ;; Mejora la navegación con ivy
 (use-package ivy
@@ -124,6 +147,111 @@
 	 ("C-x C-f" . counsel-find-file)
 	 ("C-c k" . counsel-rg)))
 
+;; Aplica company-mode para código en C y C++
+(use-package cc-mode
+  :ensure t
+  :config
+  (add-hook 'c-mode-hook 'company-mode)
+  (add-hook 'c++-mode-hook 'company-mode))
+
+;; ---------------------------------
+;; LANGUAGE PROGRAMMING
+;; ---------------------------------
+
+;; Indentación de C y C++
+(defun my-c-mode-hook()
+  (setq c-basic-offset 4)
+  (setq tab-width 4))
+;; Establece la nueva indentacion a 4 espacios
+(add-hook 'c-mode-common-hook 'my-c-mode-hook)
+
+;; Indentación de Python
+(defun my-python-mode-hook()
+  (setq python-indent-offset 4)
+  (setq tab-width 4)
+  (setq python-shell-interpreter "python3"))
+;; Establece la nueva indentacion a 4 espacios
+(add-hook 'python-mode-hook 'my-python-mode-hook)
+
+;; Configuración de Vala
+(use-package vala-mode
+  :ensure t)
+
+;; Configuración de Golang
+(use-package go-mode
+  :ensure t
+  :mode ("\\.go\\'" . go-mode)
+  :config
+  (setq-default tab-width 4)
+  (setq gofmt-command "gofmt")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq go-tab-width 4))
+
+;; Configuración de Rust
+(use-package rust-mode
+  :ensure t
+  :mode "\\.rs\\'"
+  :config
+  (setq rust-format-on-save t))
+
+;; C#-mode
+(use-package csharp-mode
+  :ensure t
+  :mode "\\.cs\\'"
+  :config
+  (setq c-basic-offset 4))
+
+;; Ruby-mode
+(use-package ruby-mode
+  :mode "\\.rb\\'"
+  :interpreter "ruby"
+  :config
+  (setq ruby-indent-level 2))
+
+;; Kotlin-mode
+(use-package kotlin-mode
+  :ensure t
+  :mode "\\.kt\\'"
+  :config
+  (setq kotlin-tab-width 4))
+
+;; Haskell-mode
+(use-package haskell-mode
+  :ensure t
+  :mode "\\.hs\\'"
+  :config
+  (setq haskell-indentation-layout-offset 4)
+  (setq haskell-indentation-starter-offset 4))
+
+;; Php-mode
+(use-package php-mode)
+
+;; Racket-mode
+(use-package racket-mode
+  :ensure t
+  :mode "\\.rkt\\'"
+  :config
+  (setq racket-indent-offset 2))
+
+;; Language Server Protocol (LSP)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook ((go-mode . lsp-deferred)
+         (rust-mode . lsp-deferred)))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;;(use-package flycheck
+;;  :ensure t
+;;  :init (global-flycheck-mode 1))
+
+;;(use-package yasnippet
+;;  :ensure t
+;;  :init (yas-global-mode 1))
+
 ;; Dracula Theme
 (use-package dracula-theme
   :ensure t
@@ -131,8 +259,64 @@
   (setq dracula-theme t)
   (load-theme 'dracula t))
 
+;; Tango Dark
+;;(load-theme 'tango-dark t)
+
+;; Zenburn Theme
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :init
+;;   (setq zenburn-theme t)
+;;   (load-theme 'zenburn t))
+
+;; Configuración de Markdown
+;; C-c C-c p (markdown-preview) Como se ve renderizado
+;; M-RET nuevo encabezado
+;; C-c C-s b: insertar texto en negritas
+;; C-c C-s i: Insertar texto en itálicas
+;; C-c C-c l: insertar un enlace
+;; C-c C-c i: insertar una imagen
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.md\\'" "\\.markdown\\'")
+  :init
+  (setq markdown-command "multimarkdown")
+  :config
+  (setq markdown-fontify-code-blocks-natively t))
+
+;; Configuraciín de Org-mode
+;; Habilitar el soporte para imágenes en línea
+(setq org-startup-with-inline-images t)
+;; Permite el resaltado de sintaxis en bloques de código
+(setq org-src-fontify-natively t)
+;; Asegura que los bloques de código se alineen correctamente
+(setq org-edit-src-content-indentation 0)
+
+
+;; Configuración de CMake
+(use-package cmake-mode
+  :ensure t
+  :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'")
+  :init
+  (setq cmake-tab-width 4)) ;; Indentación
+
+;; Colorea qué paréntesis cierra con cuál
+ ;;(use-package rainbow-delimiters
+   ;;:hook (prog-mode . rainbow-delimiters-mode))
+
+;; Ayuda para recomendar combinaciones de teclas
+(use-package which-key
+  :config (which-key-mode))
+
 ;; Autocompletar paréntesis
 (electric-pair-mode 1)
+
+;; Mostrar paréntesis coincidentes
+(show-paren-mode 1)
+
+;; Habilita la visualización de la línea actual resaltada en
+;; todos los buffers
+(global-hl-line-mode 1)
 
 ;; Habilita cambiar de ventana usando Shift + flechas
 (windmove-default-keybindings)
@@ -147,6 +331,10 @@
 ;; en lugar de un sonido
 (setq visible-bell t)
 
+;; Habilita modo de número de columna en la
+;; barra de estado
+(setq column-number-mode t)
+
 ;; Usa espacios en lugar de tabs para la indentación
 (setq-default indent-tabs-mode nil)
 
@@ -155,42 +343,22 @@
 ;; Cierra o mata el buffer *Messages* si está abierto
 (kill-buffer "*Messages*")
 
-;; Habilita la visualización de la línea actual resaltada en
-;; todos los buffers
-(global-hl-line-mode 1)
-
 (custom-set-variables
- '(inhibit-startup-screen t)
- '(package-selected-packages '(rego-mode dracula-theme haskell-mode use-package)))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cmake-mode rego-mode dracula-theme haskell-mode use-package)))
 
 (custom-set-faces
- '(hl-line ((t (:background "#483d8b")))))
-(put 'set-goal-column 'disabled nil)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:foreground "#6c757d"))))
+ '(line-number-current-line ((t (:background "#3a3c4e" :foreground "#ffffff" :weight bold))))
+ '(show-paren-match ((t (:background "#44475a" :foreground "cyan" :weight bold))))
+ '(show-paren-mismatch ((t (:background "#44475a" :foreground "orange" :weight bold)))))
 
-;; Movernos al siguiente buffer
-(global-set-key (kbd "C-x C-?") 'next-buffer)
-;; Movernos al buffer anterior
-(global-set-key (kbd "C-x C-p") 'previous-buffer)
-;; Cambiar entre buffers
-(global-set-key (kbd "C-x C-o") 'other-window-backward)
-;; Agrega un comentario, lo quita, o lo añade al final
-(global-set-key (kbd "C-x ,") 'comment-dwim)
-;; Cambia al buffer seleccionado
-(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
-;; Comenta una línea
-(global-set-key (kbd "C-c C-c") 'comment-line)
-;; Comenta una región
-(global-set-key (kbd "C-c C-r") 'comment-or-uncomment-region)
-;; Duplica una línea de código actual
-(global-set-key (kbd "C-c d") 'duplicate-line)
-
-;; Función que duplica línea de código actual
-(defun duplicate-line()
-  "Duplicate current line"
-  (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (open-line 1)
-  (next-line 1)
-  (yank))
+;;; init.el ends here
